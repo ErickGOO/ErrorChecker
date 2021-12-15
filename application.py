8c6ea15179b8b5d7bc1702940cd96acbb32e8670
Erick García Ortiz <egocv@hotmail.com>
2021-12-14 21:59:57 -0600

    Update application.py

--- application.py
+++ application.py
@@ -3,8 +3,8 @@ application = Flask(__name__)
 
 @application.route('/')
 
-def application():
+def index():
     return 'holis Esponjtas'
 
 if __name__ =="__main__":
-	application.run('0.0.0.0',port=80,debug=True)
+	application.run('0.0.0.0',port=80)
